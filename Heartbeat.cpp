b546251bfda15fdb88e56753ed3496e193f4e95b
Nils <niemann.nils@googlemail.com>
2017-04-11 19:25:55 +0200

    heartbeat with squared value

--- Heartbeat.cpp
+++ Heartbeat.cpp
@@ -1,4 +1,5 @@
 #include "Heartbeat.h"
+#include <stdio.h>
 
 /**
  *  ________
@@ -26,6 +27,7 @@ float Heartbeat::triangular( float t, float t0, float width ) {
 	return r;
 }
 
+// ____/\/\___
 
 void Heartbeat::next() {
 	Effect::next();
@@ -38,11 +40,44 @@ void Heartbeat::next() {
 	float t = (float) this->step / this->maxSteps;
 	float v = Heartbeat::triangular( t, t0, width )
 			+ Heartbeat::triangular( t, t1, width );
-	if (v > 1.f) v = 1.f;
+	//if (v > 1.f) v = 1.f;
+	this->current_r = this->r * v*v;
+	this->current_g = this->g * v*v;
+	this->current_b = this->b * v*v;
+}
+
+// ___/\  _____
+//      \/
+/*
+void Heartbeat::next() {
+	Effect::next();
+
+	const float totalWidth = 0.3;
+	const float baseLevel = 0.3;
+
+	const float up = 1. - baseLevel;
+	const float down = baseLevel;
+	const float upWidth = totalWidth * up;
+	const float downWidth = totalWidth * down;
+	const float tup = 0.5 - upWidth/2.;
+	const float tdown = 0.5 + downWidth/2.;
+
+	float t = (float) this->step / this->maxSteps;
+	float upval = Heartbeat::triangular( t, tup, upWidth );
+	float downval = Heartbeat::triangular( t, tdown, downWidth );
+	upval = upval * up;
+	downval = downval * down;
+	
+	float v = baseLevel 
+			+ upval 
+			- downval;
+
+	printf( "up: %1.4f down: %1.4f \n", upval, downval );
+
 	this->current_r = this->r * v;
 	this->current_g = this->g * v;
 	this->current_b = this->b * v;
-}
+}*/
 
 void Heartbeat::getRGB( unsigned char /*id*/,
 		unsigned char* r, unsigned char* g, unsigned char* b ) {
